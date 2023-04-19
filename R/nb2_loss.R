b2246951b71fe7e4163dc97357e2b28d32d3d53d
Francis Duval <francisduval37@gmail.com>
2023-04-19 16:58:22 -0400

    Create nb2_loss.R

---
+++ R/nb2_loss.R
@@ -0,0 +1,9 @@
+nb2_loss <- function(mu, phi, target) {
+  x1 <- target * torch_log((phi * mu) / (1 + phi * mu))
+  x2 <- (1 / phi) * torch_log(1 + phi * mu)
+  x3 <- torch_lgamma(target + 1 / phi)
+  x4 <- torch_lgamma(1 / phi)
+  
+  loss_vec <- - (x1 - x2 + x3 - x4)
+  mean(loss_vec)
+}
